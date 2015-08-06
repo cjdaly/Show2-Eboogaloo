f84e755adc816404dc4b46e8d30dd11f4424b628
cjdaly <cjdaly@locosoft.com>
2015-08-06 00:04:48 -0700

    first post

---
+++ net.locosoft.fold.channel.show2/src/net/locosoft/fold/channel/show2/internal/Show2Channel.java
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ * Copyright (c) 2015 Chris J Daly (github user cjdaly)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   cjdaly - initial API and implementation
+ ****************************************************************************/
+
+package net.locosoft.fold.channel.show2.internal;
+
+import net.locosoft.fold.channel.AbstractChannel;
+import net.locosoft.fold.channel.IChannel;
+import net.locosoft.fold.channel.show2.IShow2Channel;
+
+public class Show2Channel extends AbstractChannel implements IShow2Channel {
+
+	public Class<? extends IChannel> getChannelInterface() {
+		return IShow2Channel.class;
+	}
+
+	public void init() {
+		System.out.println("in Show2Channel.init()");
+	}
+}
